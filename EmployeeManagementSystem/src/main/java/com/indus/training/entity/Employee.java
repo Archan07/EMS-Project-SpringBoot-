6a13cd1088936946be35ee93fbd17029721670fd
Sai Archan Bommaraju <144929173+Archan07@users.noreply.github.com>
2024-09-30 13:24:58 -0400

    Add files via upload

---
+++ EmployeeManagementSystem/src/main/java/com/indus/training/entity/Employee.java
@@ -0,0 +1,66 @@
+package com.indus.training.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; 
+	
+	@Column(name = "first_Name")
+	private String firstName; 
+	
+	@Column(name = "last_Name")
+	private String lastName; 
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+	public Employee() {
+		super();
+	}
+
+	public Employee(Long id, String firstName, String lastName, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+}
---
+++ EmployeeManagementSystem/src/test/java/com/indus/training/ems/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indus.training.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
